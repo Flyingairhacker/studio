a509f7dde723eb58c5f0705e4093a59b2d1ebf3d
Rohit Sheoran <rohitsheoran376@gmail.com>
2026-01-11 07:22:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,19 @@
+// This file is used to configure the Firebase SDK.
+// It is safe to expose these values on the client side.
+// For more information on how to get these values, see:
+// https://firebase.google.com/docs/web/setup#get-config
+
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+
+if (!apiKey) {
+    throw new Error("Missing Firebase API Key. Please set NEXT_PUBLIC_FIREBASE_API_KEY in your .env.local file.");
+}
+
 export const firebaseConfig = {
-  "projectId": "studio-4587087298-4fa06",
-  "appId": "1:664675296018:web:8a9bb338f85d8ee053c9cb",
-  "apiKey": "AIzaSyAqxEQQVWr9GNnCZmp-UKFn30EUs9c9DGc",
-  "authDomain": "studio-4587087298-4fa06.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "664675296018"
+  apiKey: apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
